946420a643647f838cdab3c7cb21bc853bc0245d
Vandeilsonpp <vandeilson.nobre@picpay.com>
2024-03-23 21:27:46 -0300

    add nginx

--- src/main/java/com/example/teste/MyController.java
+++ src/main/java/com/example/teste/MyController.java
@@ -1,6 +1,7 @@
 package com.example.teste;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,6 +12,11 @@ public class MyController {
     @GetMapping("teste")
     public String myMethod() {
         return "Eita preula";
+    }
+
+    @GetMapping("mais-teste")
+    public String anotherMethod() {
+        return "Deu certo de novo";
     }
 
 }
